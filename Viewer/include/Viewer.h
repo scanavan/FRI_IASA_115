f52fe965b75f882eeb6607b080419dce41ca0a8d
scanavan <scanavan@binghamton.edu>
2017-03-13 17:39:52 -0400

    Additions to assignment.

--- Viewer/include/Viewer.h
+++ Viewer/include/Viewer.h
@@ -5,9 +5,11 @@
 class Viewer
 {
 public:
-
-
+	Viewer(cv::String title);
+	void Run();
 private:
-	
+	cv::String mTitle;
+	cv::VideoCapture capture;
+	cv::Mat image;
 };
 #endif
--- Viewer/source/Viewer.cpp
+++ Viewer/source/Viewer.cpp
@@ -1,1 +1,23 @@
 #include "Viewer.h"
+Viewer::Viewer(cv::String title) :
+	mTitle(title)
+{
+	capture = cv::VideoCapture(0);
+	capture.isOpened();
+}
+void Viewer::Run()
+{
+	cv::namedWindow(mTitle, CV_WINDOW_NORMAL);
+
+	for (;;)
+	{
+		int key = cv::waitKey(33);
+		if (key == 27)
+		{
+			break;
+		}
+		capture >> image;
+		cv::imshow(mTitle, image);
+	}
+	cv::destroyAllWindows();
+}
